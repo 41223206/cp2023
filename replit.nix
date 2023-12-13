35e800a8902168983915a9225da4a54318046f21
41223206 <144991408+41223206@users.noreply.github.com>
2023-12-13 16:43:40 +0000

    增加國旗

--- replit.nix
+++ replit.nix
@@ -1,7 +1,18 @@
 { pkgs }: {
     deps = [
+      pkgs.pandoc
+      pkgs.glibcLocales
+      pkgs.gitFull
+      pkgs.libev
       pkgs.gnuplot
       pkgs.ncurses.dev
       pkgs.gd
     ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.glibcLocales
+      pkgs.gitFull
+      pkgs.libev
+    ];
+  };
 }
